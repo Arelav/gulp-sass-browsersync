740f21902e2248d45c60d142ee9db7952fee1289
Valery Kharshats <valera.kh@gmail.com>
2014-04-08 22:24:47 +0300

    Starting refactor - moving to app folder

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,24 @@
+'use strict';
+
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var neat = require('node-neat').includePaths;
 var browserSync = require('browser-sync');
 
 gulp.task('sass', function () {
-    gulp.src('scss/**/*.scss')
-        .pipe(sass({
-            includePaths: ['scss'].concat(neat)
-        }))
-        .pipe(gulp.dest('css'));
+  return gulp.src('app/scss/**/*.scss').pipe(sass({
+      includePaths: ['app/scss'].concat(neat)
+    })).pipe(gulp.dest('app/css'));
 });
 
-gulp.task('browser-sync', function() {
-    browserSync.init(["**/*.html", "css/**/*.css", "js/**/*.js"], {
-        server: {
-            baseDir: "./"
-        }
-    });
+gulp.task('browser-sync', function () {
+  browserSync.init(['app/**/*.html', 'app/css/**/*.css', 'app/js/**/*.js'], {
+    server: {
+      baseDir: 'app/'
+    }
+  });
 });
 
 gulp.task('default', ['sass', 'browser-sync'], function () {
-    gulp.watch("scss/*.scss", ['sass']);
+  gulp.watch('app/scss/*.scss', ['sass']);
 });
